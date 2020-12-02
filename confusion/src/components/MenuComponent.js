3db109028ecae5a56c58698cc27330645430e6f2
Mohamed Haneefa <mdhaneef619@yahoo.com>
2020-12-02 17:08:48 +0530

    Assignment 1

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -37,20 +37,31 @@ class Menu extends Component {
     }
 
     render() {
-        const menu = this.props.dishes.map((dish) => {
+        const menu = this.props.dishes.map(dish => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                 <div className="row">
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            )
+        });
+        return (
+            <div className="container">
+                <div className="row">
                     {menu}
                 </div>
                 <div className="row">
                     {this.renderDish(this.state.selectedDish)}
                 </div>
                 <DishDetail dish={this.state.selectedDish} />
-              </div>
-            );
-        });
+            </div>
+        );
     }
 }
+
 
 export default Menu;
